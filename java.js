9958262a06186b73b88e28055c8ba3f6308cddea
Switchy131933 <zukagugua@gmail.com>
2023-10-27 14:42:11 +0400

    damaviwyda

--- java.js
+++ java.js
@@ -1,1 +1,11 @@
- 
+let zuka = 30
+
+if( zuka % 3 == 0 && zuka % 5 == 0){
+    console.log ("FizzBuzz") }
+      else if ( zuka % 5 == 0 ){
+        console.log("Buzz")}
+        else if (zuka % 3 == 0){
+            console.log("Fizz")
+        }else{
+            console.log("kaito")
+        }
