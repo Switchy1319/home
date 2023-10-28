48650b0067a643a51baf9be5a6f7c26a267e61a4
Switchy131933 <zukagugua@gmail.com>
2023-10-28 18:24:56 +0400

    first commit

--- java.js
+++ java.js
@@ -9,3 +9,18 @@ if( zuka % 3 == 0 && zuka % 5 == 0){
         }else{
             console.log("kaito")
         }
+
+
+
+        const brg = document.getElementById("bruger");
+        const brg1 = document.querySelector('.logoo'); 
+
+    
+        brg.addEventListener('click', function() {
+        brg1.classList.toggle('active');
+        if(brg1.classList.toggle("active")){
+            burgeristvis.display.style="block"
+        }else {
+            burgeristvis.display.style="none"
+        }
+    });
